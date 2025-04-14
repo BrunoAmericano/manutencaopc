7fb8929a34da294b1513593685c6becf508828c8
Bruno Prado <bruno.BJ684@gmail.com>
2025-04-14 07:09:35 -0300

    commitando
    foi feito o dowload dos arquivos pelo spring initializr e extraido os arquivos

---
+++ manutencaopc/src/main/java/manutencaopc/ManutencaopcApplication.java
@@ -0,0 +1,13 @@
+package manutencaopc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaopcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaopcApplication.class, args);
+	}
+
+}
