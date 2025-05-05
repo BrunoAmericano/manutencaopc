0ecc50f94b17916eee32ea37afdb9aed8a1bb284
Bruno Prado <bruno.BJ684@gmail.com>
2025-05-05 11:13:54 +0000

    editando

--- manutencaopc/src/main/java/manutencaopc/model/Peca.java
+++ manutencaopc/src/main/java/manutencaopc/model/Peca.java
@@ -1,17 +1,58 @@
 package manutencaopc.model;
 
-import jakarta.persistence.*;
-import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Peca {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
+    @Id
+    private Long id;
     private String nome;
     private String fabricante;
-    private BigDecimal preco;
+    private Double preco;
+
+    // Construtores
+    public Peca() {}
+
+    public Peca(Long id, String nome, String fabricante, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.fabricante = fabricante;
+        this.preco = preco;
+    }
 
     // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
 }
